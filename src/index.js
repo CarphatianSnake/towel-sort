598d6e929fb28bee51c596e75d750ae3ec6b542a
unknown <sunsn4ke@gmail.com>
2022-08-12 18:33:11 +0300

    refactor: make code in 2 strings

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 module.exports = function towelSort (matrix) {
-
   if (matrix === undefined) return [];
-
-  let res = [];
-
-  const resPush = arr => arr.forEach(value => res.push(value));
-
-  matrix.forEach((item, i) => {
-    if (i % 2 === 0) {
-      resPush(item);
-    } else {
-      resPush(item.reverse());
-    }
-  });
-
-  return res;
+  return matrix.map((item, i) => i % 2 === 0 ? item : item.reverse()).flat();
 }
